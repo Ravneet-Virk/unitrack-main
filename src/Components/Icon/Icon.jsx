0f269d52894f62f0ddddd2a7c043fe5d83cecad5
Ravneet-Virk <ravneetvirk230@gmail.com>
2024-04-15 15:24:09 +0530

    message

--- src/Components/Icon/Icon.jsx
+++ src/Components/Icon/Icon.jsx
@@ -4,7 +4,7 @@ import "./Icon.css"
 const Icon = () => {
     return (
         <div className='Icon-div'>
-           <a href='https://wa.me/917710777666?text=Hey! Its Ravneet Here' target='_blank'><img src='./images/WA-Logo.png' alt='Image' /></a> 
+           <a href={`Hi, I'm checking out visas. Can you tell me how your consultations work and how much they cost?`} target='_blank'><img src='./images/WA-Logo.png' alt='Image' /></a> 
         </div>
     )
 }
