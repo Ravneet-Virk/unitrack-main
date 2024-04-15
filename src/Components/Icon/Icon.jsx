438a8dc0d318c7bcdb80b8e7a7b52b7aba11cb52
Ravneet-Virk <ravneetvirk230@gmail.com>
2024-04-15 11:44:53 +0530

    WhatsappIcon

---
+++ src/Components/Icon/Icon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Icon.css"
+
+const Icon = () => {
+    return (
+        <div className='Icon-div'>
+           <a href='https://wa.me/917710420851?text=Hey! Its Ravneet Here' target='_blank'><img src='./images/WA-Logo.png' alt='Image' /></a> 
+        </div>
+    )
+}
+
+export default Icon
